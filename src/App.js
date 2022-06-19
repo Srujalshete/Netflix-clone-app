6c4b6db273b916c461965361f18bac19b6db4ca5
srujalshete <sujalshete13@gmail.com>
2022-06-19 13:55:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+ 
+import './App.css';
+import Banner from './Components/Banner';
+import Navbar from './Components/Navbar';
+import Row from './Components/Row';
+import requests from './request';
+
+function App() {
+  return (
+    <div className="app">
+       <Navbar />
+       <Banner />
+       <h1>Netflic clone</h1>
+
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}
+      isLargeRow />
+      <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+
+    </div>
+  );
+}
+
+export default App;
