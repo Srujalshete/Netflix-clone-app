f75251dd26f0d4b7ad6efd618dd61b1a9e5f4955
Srujal Shete <67551839+Srujalshete@users.noreply.github.com>
2022-07-22 00:46:34 +0530

    Update Row.js

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -2,27 +2,25 @@ import React, { useEffect, useState } from 'react';
 import './Row.css';
 import axios from '../axios';
 import YouTube from 'react-youtube';
-import movieTrailer from 'movie-trailer';
-
-
+import movieTrailer from 'movie-trailer'
 
 const baseUrl = "https://image.tmdb.org/t/p/original/";
 
-function Row({title , fetchUrl ,  isLargeRow }) {
-    const [movies, setMovies] = useState([]);
+function Row({ title , fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  
+  const [trailerUrl, setTrailerUrl] = useState("");
 
-    const [trailerUrl, setTrailerUrl] = useState("");
-
-    useEffect(() => {
-        async function fetchData(){
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
-            return request;   
-        }fetchData();
-    }, [fetchUrl]);
-
-
-    const opts = {
+  useEffect(() => {
+    async function fetchData(){
+     const request = await axios.get(fetchUrl);
+     setMovies(request.data.results);
+     return request;
+    } fetchData();
+    
+  }, [fetchUrl]);
+ 
+   const opts = {
      height: "390",
      width: "100%",
      playerVars: {
@@ -38,31 +36,34 @@ function Row({title , fetchUrl ,  isLargeRow }) {
        .then(url => {
          const urlParams = new URLSearchParams( new URL(url).search);
          setTrailerUrl(urlParams.get('v'));
-         }).catch(error => console.log(error))
+
+       }).catch(error => console.log(error))
      }
        };
-    return(
-        <div className="row">
 
-        <h2>
-        {title}
-        </h2>
-        <div className="row_posters">
-             {movies.map(movie => (
+    return (
+        <div className="row" >
+            <h2>{title}</h2>
+
+            <div className="row__posters" >
+
+              {movies?.map(movie =>(
                 <img 
                 key={movie.id}
                 onClick={()=> handleClick(movie)}
                 className={`row__poster  ${isLargeRow && "row__posterLarge"} `}
-                src={`${baseUrl}${isLargeRow ? movie?.poster_path : movie?.backdrop_path} `}
+                src={`${baseUrl}${isLargeRow ? movie?.poster_path : movie?.backdrop_path}`}
                 alt={movie.name} 
+                
                 />
               ))}
-        </div>
-         {trailerUrl && <YouTube
+
+            </div>
+            {trailerUrl && <YouTube
              videoId={trailerUrl}  
              opts={opts}
             />}
-    </div>
+        </div>
     )
 }
 
