37bef6c8fb1afe9fe3c1a4c3ca3ae9a3ccaf0845
Srujal Shete <67551839+Srujalshete@users.noreply.github.com>
2022-07-22 00:42:13 +0530

    Update request.js

--- src/request.js
+++ src/request.js
@@ -1,6 +1,6 @@
 const API_KEY = "07cc0846c9e5c411624fad8852823a23";
 
-const requests ={
+const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US` ,
